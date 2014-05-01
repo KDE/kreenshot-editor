eb51f83f5b3ef2ffb8250507fe1d25e6883fbf7b
Gregor Mi <codeminister@publicstatic.de>
2014-05-01 19:54:45 +0200

    add commandline parsing

--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,76 @@
 #include <QCommandLineOption>
 #include <QDebug>
 
+#include <iostream>
+#include <stdexcept>
+
+#define tr(arg) QObject::tr(arg)
+
 int main(int argc, char *argv[])
 {
+    const QString version = "0.1";
+
     QApplication app(argc, argv);
+
+    QCoreApplication::setApplicationVersion(version);
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.setApplicationDescription("Image editor for screenshots");
+
+    parser.addPositionalArgument("image", tr("file path to primary image to be loaded"), tr("imagefile"));
+
+    //QCommandLineOption deviceOption(QStringList() << "i" << "image", "file path of image to be loaded", "file path");
+    QCommandLineOption image2Option(QStringList() << tr("image2"), tr("file path to (optional) secondary image to be inserted as object"), tr("file path"));
+    parser.addOption(image2Option);
+
+    QCommandLineOption image2xOption(QStringList() << tr("image2x"), tr("x coordinate (upper left corner) of the secondary image. Default value is 0."), tr("x in pixel"));
+    parser.addOption(image2xOption);
+
+    QCommandLineOption image2yOption(QStringList() << tr("image2y"), tr("y coordinate. Default value is 0."), tr("y in pixel"));
+    parser.addOption(image2yOption);
+
+    parser.process(app);
+
+    const QStringList args = parser.positionalArguments();
+
+    try {
+        if (args.length() != 1) {
+            // http://stackoverflow.com/questions/4214369/how-to-convert-qstring-to-stdstring
+            throw std::runtime_error("1 positional argument expected");
+        }
+
+        QString imageFilepath = args[0];
+        qDebug() << "imageFilepath:" << imageFilepath;
+
+        QString image2Filepath = parser.value(image2Option);
+        qDebug() << "image2Filepath:" << image2Filepath;
+
+        QString image2xStr = parser.value(image2xOption);
+        qDebug() << "image2x:" << image2xStr;
+        int image2x = 0;
+        if (!image2xStr.isEmpty()) {
+            bool ok;
+            image2x = image2xStr.toInt(&ok);
+            if (!ok) throw std::runtime_error("wrong argument (cannot parse x value)");
+        }
+        qDebug() << "image2x:" << image2x;
+
+        QString image2yStr = parser.value(image2yOption);
+        qDebug() << "image2y:" << image2yStr;
+        int image2y = 0;
+        if (!image2yStr.isEmpty()) {
+            bool ok;
+            image2y = image2yStr.toInt(&ok);
+            if (!ok) throw std::runtime_error("wrong argument (cannot parse y value)");
+        }
+        qDebug() << "image2y:" << image2y;
+    }
+    catch (const std::runtime_error& e) {
+        std::cout << tr("Error with command line usage: ").toUtf8().constData() << e.what() << std::endl << std::endl;
+        parser.showHelp(1);
+    }
+
 
     return app.exec();
 }
