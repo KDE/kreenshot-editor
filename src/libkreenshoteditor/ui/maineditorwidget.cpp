7c4111e3541957b059ce89250a0a24a8243b3494
Gregor Mi <codeminister@publicstatic.de>
2014-05-10 17:59:35 +0200

    cr

--- src/libkreenshoteditor/ui/maineditorwidget.cpp
+++ src/libkreenshoteditor/ui/maineditorwidget.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the kreenshot-editor project.
  *
- * Copyright (C) 2014 by Gregor Mitsch <codeminister@publicstatic.de>
+ * Copyright (C) 2014 by Gregor Mi <codeminister@publicstatic.de>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
