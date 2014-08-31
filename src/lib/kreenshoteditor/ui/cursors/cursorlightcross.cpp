5e045dddfdb42e7ad1010ea8ceef1f2cb2b16619
Gregor Mi <codestruct@posteo.org>
2014-08-31 18:07:59 +0200

    compile cursors

--- src/lib/kreenshoteditor/ui/cursors/cursorlightcross.cpp
+++ src/lib/kreenshoteditor/ui/cursors/cursorlightcross.cpp
@@ -1,3 +1,21 @@
+/*
+ * This file is part of the kreenshot-editor project.
+ *
+ * Copyright (C) 2014 by Gregor Mi <codestruct@posteo.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 or the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
 
 /*
    Copyright (c) 2003-2007 Clarence Dang <dang@kde.org>
@@ -27,14 +45,9 @@
 
 #define DEBUG_KP_CURSOR_LIGHT_CROSS 0
 
-
-#include <kpCursorLightCross.h>
-
+#include "cursorlightcross.h"
 #include <qbitmap.h>
 #include <qcursor.h>
-
-#include <kdebug.h>
-
 
 enum PixelValue
 {
--- src/lib/kreenshoteditor/ui/cursors/cursorlightcross.h
+++ src/lib/kreenshoteditor/ui/cursors/cursorlightcross.h
@@ -1,3 +1,21 @@
+/*
+ * This file is part of the kreenshot-editor project.
+ *
+ * Copyright (C) 2014 by Gregor Mi <codestruct@posteo.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 or the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
 
 /*
    Copyright (c) 2003-2007 Clarence Dang <dang@kde.org>
@@ -25,15 +43,11 @@
    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-
 #ifndef KP_CURSOR_LIGHT_CROSS_H
 #define KP_CURSOR_LIGHT_CROSS_H
 
-
 class QCursor;
 
-
 const QCursor *kpCursorLightCrossCreate ();
-
 
 #endif  // KP_CURSOR_LIGHT_CROSS_H
